880289d7c3b52680bcef6791521db454b9e5efd9
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-02 08:25:42 -0300

    feat: add sendEmail function

--- src/index.js
+++ src/index.js
@@ -12,10 +12,5 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-
-app.listen(process.env.PORT, ()=> console.log(`Server listening at ${process.env.PORT}`))
-
- console.log(process.env.PORT)
-
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`servidor iniciado na porta ${port}`))
