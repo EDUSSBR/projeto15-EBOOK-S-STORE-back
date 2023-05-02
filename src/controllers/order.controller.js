960888eab264ec5ee5f011305e94f09537e1a3a3
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-02 15:21:52 -0300

    feat: add sendEmail functionality in orders endpoint

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,10 +1,12 @@
 import { db } from "../db/database.js"
+import { sendEmail } from "../helpers/email.js"
 
 export async function order(req, res){
-    const {name, email ,cart, paymentForm, total} = req.body
+    const {name, email ,cart, paymentForm, total, id} = req.body
     console.log(name)
     try {
         await db.collection("order").insertOne({name, email ,cart, paymentForm, total})
+        await sendEmail(name, email ,cart, paymentForm, id)
         res.send(200)
     } catch (err) {
         res.status(500).send(err.message)
--- src/helpers/email.js
+++ src/helpers/email.js
@@ -6,21 +6,35 @@ const sgMail = new MailService()
 console.log(process.env.SENDGRID_API_KEY)
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-export async function sendEmail(email, name, paymentType, cartItems, orderNumber) {
+export async function sendEmail(name, email, cart, paymentForm, orderNumber) {
   try {
     let str = "";
     let total = 0;
-    for (let item of cartItems) {
+    let arrItems = []
+    for (let item of cart) {
       total += Number(item.price) * Number(item.quantity)
-      str += `<p>${item.name}: R$${item.price}</p> `
+      str += `
+    <tr>
+    <td><strong>${item.name}   </strong> </td>
+    <td style={text-align:center} ><strong>${item.quantity}</strong> </td>
+    <td><strong>${item.price}</strong> </td>
+    </tr>
+    `
     }
 
     const message = `
     <h1>${name}, obrigado por seu pedido.</h1> 
     <p>Confirmamos que recebemos seu pedido #${orderNumber} e estamos trabalhando para processá-lo o mais rápido possível.</p>
     <h2>Dados do pedido:</h2>
+    <table>
+    <tr>
+    <th>Livro</th>
+    <th>Qt.</th>
+    <th>Preço(R$)</th>
+    </tr>
     ${str}
-    <p>Forma de pagamento: ${paymentType}</p>
+    </table>
+    <p>Forma de pagamento: ${paymentForm}</p>
     <p><strong>Total: R$${total},00</strong></p> 
     `
     const msg = {
